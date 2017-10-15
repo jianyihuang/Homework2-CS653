caa41d3bfe100de259428957adb9ae7658e12dec
JianYi Huang <jianyihuang@JianYis-MacBook-Pro.local>
2017-10-14 21:34:25 -0400

    add files

---
+++ experiment1.tcl
@@ -0,0 +1,43 @@
+#Create a simulator object
+set ns [new Simulator]
+
+#Open the nam trace file
+set nf [open out.nam w]
+$ns namtrace-all $nf
+
+#Define a 'finish' procedure
+proc finish {} {
+        global ns nf
+        $ns flush-trace
+	#Close the trace file
+        close $nf
+	#Execute nam on the trace file
+        exec nam out.nam &
+        exit 0
+}
+
+#Create two nodes
+set n1 [$ns node]
+set n2 [$ns node]
+set n3 [$ns node]
+set n4 [$ns node]
+set n5 [$ns node]
+set n6 [$ns node]
+#Create a duplex link between the nodes
+$ns duplex-link $n1 $n2 1Mb 10ms DropTail
+$ns duplex-link $n2 $n5 1Mb 10ms DropTail
+$ns duplex-link $n2 $n3 1Mb 10ms DropTail
+$ns duplex-link $n3 $n4 1Mb 10ms DropTail
+$ns duplex-link $n3 $n6 1Mb 10ms DropTail
+
+$ns duplex-link-op $n1 $n2 orient right-down
+$ns duplex-link-op $n5 $n2 orient right-up
+$ns duplex-link-op $n2 $n3 orient right
+$ns duplex-link-op $n3 $n4 orient right-up
+$ns duplex-link-op $n3 $n6 orient right-down
+
+#Call the finish procedure after 5 seconds of simulation time
+$ns at 5.0 "finish"
+
+#Run the simulation
+$ns run
