e97acbdf21ec25444f1f092dd5850aa5da87b1c1
JianYi Huang <jianyihuang@eduroamguest-3-108.sys.cs.local>
2017-10-16 11:31:55 -0400

    added experiment 2

--- experiment2.tcl
+++ experiment2.tcl
@@ -30,49 +30,47 @@ $ns duplex-link $n2 $n3 1Mb 10ms DropTail
 $ns duplex-link $n3 $n4 1Mb 10ms DropTail
 $ns duplex-link $n3 $n6 1Mb 10ms DropTail
 
-#Create a UDP agent and attach it to node n0
-set udp0 [new Agent/UDP]
-$ns attach-agent $n2 $udp0
-
-# Create a CBR traffic source and attach it to udp0
-set cbr0 [new Application/Traffic/CBR]
-$cbr0 set packetSize_ 500
-$cbr0 set interval_ 0.005
-$cbr0 attach-agent $udp0
-
-set null0 [new Agent/Null]
-$ns attach-agent $n3 $null0
-
-$ns connect $udp0 $null0
-
 
 
 #set up a TCP connection from node 1 to node 4
-set tcp [new Agent/TCP/Vegas]   
-$tcp set class_ 2
-$ns attach-agent $n1 $tcp
+set tcp0 [new Agent/TCP/Reno]
+$tcp0 set class_ 2
+$ns attach-agent $n1 $tcp0
 set sink [new Agent/TCPSink]
 $ns attach-agent $n4 $sink
-$ns connect $tcp $sink
-$tcp set fid_ 1
+$ns connect $tcp0 $sink
+$tcp0 set fid_ 1
 
 #set up a FTP over TCP connection
-set ftp [new Application/FTP]
-$ftp attach-agent $tcp
-$ftp set type_ FTP
+set ftp0 [new Application/FTP]
+$ftp0 attach-agent $tcp0
+$ftp0 set type_ FTP
 
+#set up a TCP connection from node 5 to node 6
+set tcp1 [new Agent/TCP/Reno]
+$tcp1 set class_ 2
+$ns attach-agent $n5 $tcp1
+set sink [new Agent/TCPSink]
+$ns attach-agent $n6 $sink
+$ns connect $tcp1 $sink
+$tcp1 set fid_ 1
 
+#set up a FTP over TCP connection
+set ftp1 [new Application/FTP]
+$ftp1 attach-agent $tcp1
+$ftp1 set type_ FTP
 
+#set the topology
 $ns duplex-link-op $n1 $n2 orient right-down
 $ns duplex-link-op $n5 $n2 orient right-up
 $ns duplex-link-op $n2 $n3 orient right
 $ns duplex-link-op $n3 $n4 orient right-up
 $ns duplex-link-op $n3 $n6 orient right-down
 
-$ns at 0.5 "$cbr0 start"
-$ns at 4.5 "$cbr0 stop"
-$ns at 0.5 "$ftp start"
-$ns at 4.5 "$ftp stop"
+$ns at 0.5 "$ftp0 start"
+$ns at 4.5 "$ftp0 stop"
+$ns at 0.5 "$ftp1 start"
+$ns at 4.5 "$ftp1 stop"
 
 #Call the finish procedure after 5 seconds of simulation time
 $ns at 5.0 "finish"
