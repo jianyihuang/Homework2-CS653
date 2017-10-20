73de48232ff787b2be11bbf50707431631b89468
nujhedtozu <junior.tzou@gmail.com>
2017-10-20 13:46:37 -0400

    edit experiments

--- experiment1.tcl
+++ experiment1.tcl
@@ -1,13 +1,17 @@
 #Create a simulator object
 set ns [new Simulator]
 
+#Define different colors for data flows (for NAM)
+$ns color 1 Blue
+$ns color 2 Red
+$ns color 3 Green
+
 #Open the nam trace file
 set nf [open out.nam w]
 $ns namtrace-all $nf
 
-
 #Open the trace file (start)
-set tf [open my_experiment_output.tr w]
+set tf [open experiment1.tr w]
 $ns trace-all $tf
 
 #Define a 'finish' procedure
@@ -23,7 +27,6 @@ proc finish {} {
 }
 
 
-
 #Create two nodes
 set n1 [$ns node]
 set n2 [$ns node]
@@ -31,33 +34,34 @@ set n3 [$ns node]
 set n4 [$ns node]
 set n5 [$ns node]
 set n6 [$ns node]
-#Create a duplex link between the nodes
-$ns duplex-link $n1 $n2 1Mb 10ms DropTail
-$ns duplex-link $n2 $n5 1Mb 10ms DropTail
-$ns duplex-link $n2 $n3 1Mb 10ms DropTail
-$ns duplex-link $n3 $n4 1Mb 10ms DropTail
-$ns duplex-link $n3 $n6 1Mb 10ms DropTail
 
-#Create a UDP agent and attach it to node n0
+#Create a duplex link between the nodes
+$ns duplex-link $n1 $n2 10Mb 10ms DropTail
+$ns duplex-link $n2 $n5 10Mb 10ms DropTail
+$ns duplex-link $n2 $n3 10Mb 10ms DropTail
+$ns duplex-link $n3 $n4 10Mb 10ms DropTail
+$ns duplex-link $n3 $n6 10Mb 10ms DropTail
+
+#Create a UDP agent and attach it to node n2
 set udp0 [new Agent/UDP]
 $ns attach-agent $n2 $udp0
+set null0 [new Agent/Null]
+$ns attach-agent $n3 $null0
+$ns connect $udp0 $null0
+$udp0 set fid_ 3
 
 # Create a CBR traffic source and attach it to udp0
 set cbr0 [new Application/Traffic/CBR]
-$cbr0 set packetSize_ 500
-$cbr0 set interval_ 0.005
+#$cbr0 set packetSize_ 500
+#$cbr0 set interval_ 0.005
+$cbr0 set packet_size_ 1000
+$cbr0 set rate_ 1mb
 $cbr0 attach-agent $udp0
-
-set null0 [new Agent/Null]
-$ns attach-agent $n3 $null0
-
-$ns connect $udp0 $null0
-
-
+$cbr0 set type_ CBR
 
 #set up a TCP connection from node 1 to node 4
-set tcp [new Agent/TCP/Reno]
-$tcp set class_ 2
+set tcp [new Agent/TCP]
+$tcp set class_ 1
 $ns attach-agent $n1 $tcp
 set sink [new Agent/TCPSink]
 $ns attach-agent $n4 $sink
@@ -69,8 +73,7 @@ set ftp [new Application/FTP]
 $ftp attach-agent $tcp
 $ftp set type_ FTP
 
-
-
+#set the topology
 $ns duplex-link-op $n1 $n2 orient right-down
 $ns duplex-link-op $n5 $n2 orient right-up
 $ns duplex-link-op $n2 $n3 orient right
@@ -84,7 +87,6 @@ $ns at 4.5 "$ftp stop"
 
 #Call the finish procedure after 5 seconds of simulation time
 $ns at 5.0 "finish"
-
 
 #Run the simulation
 $ns run
--- experiment2.tcl
+++ experiment2.tcl
@@ -1,16 +1,26 @@
 #Create a simulator object
 set ns [new Simulator]
 
+#Define different colors for data flows (for NAM)
+$ns color 1 Blue
+$ns color 2 Red
+$ns color 3 Green
+
 #Open the nam trace file
 set nf [open out.nam w]
 $ns namtrace-all $nf
 
+#Open the trace file (start)
+set tf [open experiment2.tr w]
+$ns trace-all $tf
+
 #Define a 'finish' procedure
 proc finish {} {
-        global ns nf
+        global ns nf tf
         $ns flush-trace
 	#Close the trace file
         close $nf
+        close $tf
 	#Execute nam on the trace file
         exec nam out.nam &
         exit 0
@@ -23,18 +33,34 @@ set n3 [$ns node]
 set n4 [$ns node]
 set n5 [$ns node]
 set n6 [$ns node]
+
 #Create a duplex link between the nodes
-$ns duplex-link $n1 $n2 1Mb 10ms DropTail
-$ns duplex-link $n2 $n5 1Mb 10ms DropTail
-$ns duplex-link $n2 $n3 1Mb 10ms DropTail
-$ns duplex-link $n3 $n4 1Mb 10ms DropTail
-$ns duplex-link $n3 $n6 1Mb 10ms DropTail
+$ns duplex-link $n1 $n2 10Mb 10ms DropTail
+$ns duplex-link $n2 $n5 10Mb 10ms DropTail
+$ns duplex-link $n2 $n3 10Mb 10ms DropTail
+$ns duplex-link $n3 $n4 10Mb 10ms DropTail
+$ns duplex-link $n3 $n6 10Mb 10ms DropTail
 
+#Create a UDP agent and attach it to node n2
+set udp0 [new Agent/UDP]
+$ns attach-agent $n2 $udp0
+set null0 [new Agent/Null]
+$ns attach-agent $n3 $null0
+$ns connect $udp0 $null0
+$udp0 set fid_ 3
 
+# Create a CBR traffic source and attach it to udp0
+set cbr0 [new Application/Traffic/CBR]
+#$cbr0 set packetSize_ 500
+#$cbr0 set interval_ 0.005
+$cbr0 set packet_size_ 1000
+$cbr0 set rate_ 1mb
+$cbr0 attach-agent $udp0
+$cbr0 set type_ CBR
 
 #set up a TCP connection from node 1 to node 4
 set tcp0 [new Agent/TCP/Reno]
-$tcp0 set class_ 2
+$tcp0 set class_ 1
 $ns attach-agent $n1 $tcp0
 set sink [new Agent/TCPSink]
 $ns attach-agent $n4 $sink
@@ -53,7 +79,7 @@ $ns attach-agent $n5 $tcp1
 set sink [new Agent/TCPSink]
 $ns attach-agent $n6 $sink
 $ns connect $tcp1 $sink
-$tcp1 set fid_ 1
+$tcp1 set fid_ 2
 
 #set up a FTP over TCP connection
 set ftp1 [new Application/FTP]
@@ -67,6 +93,8 @@ $ns duplex-link-op $n2 $n3 orient right
 $ns duplex-link-op $n3 $n4 orient right-up
 $ns duplex-link-op $n3 $n6 orient right-down
 
+$ns at 0.5 "$cbr0 start"
+$ns at 4.5 "$cbr0 stop"
 $ns at 0.5 "$ftp0 start"
 $ns at 4.5 "$ftp0 stop"
 $ns at 0.5 "$ftp1 start"
