3ee53b663dab55a208a30ae713b5a6e981fd13f3
nujhedtozu <junior.tzou@gmail.com>
2017-10-20 20:38:04 -0400

    edit experiment 3

--- experiment3.tcl
+++ experiment3.tcl
@@ -1,6 +1,11 @@
 #Create a simulator object
 set ns [new Simulator]
 
+#Define different colors for data flows (for NAM)
+$ns color 1 Blue
+$ns color 2 Red
+$ns color 3 Green
+
 #Open the nam trace file
 set nf [open out.nam w]
 $ns namtrace-all $nf
@@ -23,6 +28,7 @@ set n3 [$ns node]
 set n4 [$ns node]
 set n5 [$ns node]
 set n6 [$ns node]
+
 #Create a duplex link between the nodes
 $ns duplex-link $n1 $n2 1Mb 10ms DropTail
 $ns duplex-link $n2 $n5 1Mb 10ms DropTail
@@ -30,26 +36,27 @@ $ns duplex-link $n2 $n3 1Mb 10ms DropTail
 $ns duplex-link $n3 $n4 1Mb 10ms DropTail
 $ns duplex-link $n3 $n6 1Mb 10ms DropTail
 
-#Create a UDP agent and attach it to node n0
+#Create a UDP agent and attach it to node n2
 set udp0 [new Agent/UDP]
 $ns attach-agent $n5 $udp0
+set null0 [new Agent/Null]
+$ns attach-agent $n6 $null0
+$ns connect $udp0 $null0
+$udp0 set fid_ 3
 
 # Create a CBR traffic source and attach it to udp0
 set cbr0 [new Application/Traffic/CBR]
-$cbr0 set packetSize_ 500
-$cbr0 set interval_ 0.005
+#$cbr0 set packetSize_ 500
+#$cbr0 set interval_ 0.005
+#$cbr0 set packet_size_ 2000
+$cbr0 set rate_ 1Mb
 $cbr0 attach-agent $udp0
-
-set null0 [new Agent/Null]
-$ns attach-agent $n6 $null0
-
-$ns connect $udp0 $null0
-
+$cbr0 set type_ CBR
 
 
 #set up a TCP connection from node 1 to node 4
-set tcp [new Agent/TCP/Vegas]
-$tcp set class_ 2
+set tcp [new Agent/TCP/Reno]
+$tcp set class_ 1
 $ns attach-agent $n1 $tcp
 set sink [new Agent/TCPSink]
 $ns attach-agent $n4 $sink
@@ -62,14 +69,13 @@ $ftp attach-agent $tcp
 $ftp set type_ FTP
 
 
-
 $ns duplex-link-op $n1 $n2 orient right-down
 $ns duplex-link-op $n5 $n2 orient right-up
 $ns duplex-link-op $n2 $n3 orient right
 $ns duplex-link-op $n3 $n4 orient right-up
 $ns duplex-link-op $n3 $n6 orient right-down
 
-$ns at 0.5 "$cbr0 start"
+$ns at 1.5 "$cbr0 start"
 $ns at 4.5 "$cbr0 stop"
 $ns at 0.5 "$ftp start"
 $ns at 4.5 "$ftp stop"
